3df930a542ff8c6baa2ae5f61d8c991066d87df8
Olav Valle <olavvalle@gmail.com>
2021-05-06 21:23:33 +0200

    changes implementation of client.js to function with the new Spring REST api. addItem now receives the new item back from the API if creation was successful, which it then appends to the store. This removes the need to re-fetch the entire item DB list.

--- frontend/src/features/items/ItemCardComp.js
+++ frontend/src/features/items/ItemCardComp.js
@@ -1,14 +1,13 @@
-import React, {useEffect, useState} from "react";
-import {getItemById} from "../../app/client";
+import React, {useState} from "react";
 import {itemDelete} from "./itemsSlice";
 import {useDispatch} from "react-redux";
 
 // ### PLACEHOLDER COMPONENT ###
 // Has basic example implementations of state logic and Redux interaction
-const ItemCard = ({itemId}) => {
-
+const ItemCard = ({itemAsProp}) => {
     const dispatch = useDispatch();
-    const [item, setItem] = useState(null);
+    const [item, setItem] = useState(itemAsProp);
+
 
     // problem: When an item is deleted, it seems like maybe
     // this useEffect method is called again on the re-render
@@ -21,24 +20,26 @@ const ItemCard = ({itemId}) => {
     // we need to ensure that the re-render is only triggered once the store
     // has been updated with the new item list from the server.
 
-    useEffect(() => {
-        // since this is an effect hook, we have to do some hackery.
-        // since useEffect expects a cleanup function as callback,
-        // we make the callback an IIFE to avoid returning the Promise
-        // produced by the async/await function
-        (async () => {
-            try {
-                // we must await this expression,
-                // because getItemById is promise based.
-                // Without await, we end up with setItem(Promise<any>)
-                // instead of the resolved response body.
-                let item = await getItemById(itemId);
-                setItem(item);
-            } catch (e) {
-            }
-        })() // The (expression...)() syntax makes it so that
-        // whatever is inside the first parens
-    }, [itemId])
+    // This is an example of how to use the useEffect hook to fetch
+    // item specific data from the API
+    // useEffect(() => {
+    //     // since this is an effect hook, we have to do some hackery.
+    //     // since useEffect expects a cleanup function as callback,
+    //     // we make the callback an IIFE to avoid returning the Promise
+    //     // produced by the async/await function
+    //     (async () => {
+    //         try {
+    //             // we must await this expression,
+    //             // because getItemById is promise based.
+    //             // Without await, we end up with setItem(Promise<any>)
+    //             // instead of the resolved response body.
+    //             let item = await getItemById(itemId);
+    //             setItem(item);
+    //         } catch (e) {
+    //         }
+    //     })() // The (expression...)() syntax makes it so that
+    //     // whatever is inside the first parens
+    // }, [itemId])
 
     const onDelete = (id) => {
         dispatch(itemDelete(id))
