ac8273541f737bc68b96b51f3db19327b03692a7
NekoFlaa <NekoFlaa@gmail.com>
2021-05-06 21:23:41 +0200

    Start Grid view

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,22 @@
 import React from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+import { ItemGrid } from './features/items/ItemGridComp';
+
 
 function App() {
+  // for testing only
   return (
     <div className="App">
+      <Router>
+        <Route exact path="/">
+          <ItemGrid />
+        </Route>
+      </Router>
     </div>
   );
 }
--- frontend/src/features/items/ItemGridComp.js
+++ frontend/src/features/items/ItemGridComp.js
@@ -3,12 +3,44 @@ import {getProduct} from "../../app/API";
 
 
 export const ItemGrid = () => {
-
-    const products = [];
+    // temp debug items: 
+    const products = [
+        {
+            name:"Product1",
+            desc:"insert description here",
+            imageSrc:"imageSrc"
+        },
+        {
+            name:"Product2",
+            desc:"insert description here",
+            imageSrc:"imageSrc"
+        }
+    ];
 
 
     return (
         <div>
+            <div>
+                <input type="text" name="itemSearch" id="itemSearch" />
+                <div className="viewSelect">
+                    <button name="gridView">G</button>
+                    <button name="listView">L</button>
+                </div>
+            </div>
+            <h3>This is a list of items a person (or bot) can buy from our store (if it was real)</h3>
+            <section className="shopItems" name="shop items">
+                {
+                    products.map((item) => (
+                        <div>
+                            <img src={item.imageSrc} />
+                            <h4>{item.name}</h4>
+                            <p>{item.desc}</p>
+                            <button name="add to cart">+</button>
+                        </div>
+                    ))
+                }
+                <div className="add a grid"><p>I am an item, not a tea pot</p></div>
+            </section>
 
         </div>
     )
