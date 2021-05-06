8a95e1ad3f3ff14134ea56dd46b92bd64269c5c5
Olav Valle <olavvalle@gmail.com>
2021-05-06 20:33:40 +0200

    Implements HATEOAS based RESTful API for Item database entity.

---
+++ backend/src/main/java/no/ntnu/exam2021/backend/item/Item.java
@@ -0,0 +1,49 @@
+package no.ntnu.exam2021.backend.item;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Positive;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Entity
+@NoArgsConstructor
+public class Item {
+
+  @Id @GeneratedValue(strategy = GenerationType.AUTO)
+  private Long id;
+
+  @NotBlank(message = "Name cannot be blank.")
+  private String name;
+
+  @Positive(message = "Price cannot be negative number.")
+  @NotNull(message = "Price cannot be empty.")
+  private int price;
+
+//  @NotNull(message = "Description cannot be null.")
+  private String description;
+
+  //todo: image url?
+
+
+  public Item(
+      @NotBlank(message = "Name cannot be blank.")
+          String name,
+      @Positive(message = "Price cannot be negative number.")
+      @NotNull(message = "Price cannot be empty.")
+          int price,
+      String description){
+    this.name = name;
+    this.price = price;
+    this.description = description;
+  }
+
+  public Long getId() {
+    return this.id;
+  }
+}
