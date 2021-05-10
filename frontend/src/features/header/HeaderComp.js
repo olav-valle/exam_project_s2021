4d050b30a65ba4bf0934ee599d785cbb6b592f00
Olav Valle <olavval@stud.ntnu.no>
2021-05-10 16:21:34 +0200

    Implements responsive Header/Nav. Adds react-responsive as project dependency. The Nav in the header will now shrink down to only the Home and Cart elements on screens smaller than 1024px wide. The remaining Nav links are placed in a dropdown burger menu on the right hand side.

--- frontend/src/features/header/HeaderComp.js
+++ frontend/src/features/header/HeaderComp.js
@@ -1,8 +1,34 @@
-import React from 'react';
+import React, {useState} from 'react';
 import '../../App.css';
 import {Link} from "react-router-dom";
+import {useMediaQuery} from 'react-responsive'
 
 function HeaderComp() {
+
+    const [menuState, setMenuState] = useState(false);
+
+    const isSmallScreen = useMediaQuery({query: '(max-width: 1024px)'})
+
+    const toggleMenuOnClick = (e) => {
+        // e.preventDefault();
+        // when shown, attach document listener
+        // when hidden, detach doc listener
+        setMenuState(!menuState);
+
+        if(!menuState){
+            e.stopPropagation()
+
+            document.removeEventListener('click', closeMenu);
+        }
+        if (menuState) {
+            e.stopPropagation()
+            document.addEventListener('click', closeMenu);
+        }
+    }
+    const closeMenu = ()=> {
+        setMenuState(false);
+    }
+
     return (
         <nav className="flex justify-center items-center bg-yellow h-20 text-xl">
 
@@ -33,17 +59,6 @@ function HeaderComp() {
                 </li>
                 <li>
                     <Link
-                        title="Link to About Us page"
-                        className="
-                            focus:ring-4
-                            focus:ring-gray-300"
-                        to='/about'
-                    >
-                        About
-                    </Link>
-                </li>
-                <li>
-                    <Link
                         title="Link to shopping cart"
                         className="
                             focus:ring-4
@@ -53,29 +68,114 @@ function HeaderComp() {
                         Cart
                     </Link>
                 </li>
-                <li>
-                    <Link
-                        title="Link to login page"
-                        className="
+                {isSmallScreen ? null :
+                    <React.Fragment>
+                        <li>
+                            <Link
+                                title="Link to login page"
+                                className="
                             focus:ring-4
                             focus:ring-gray-300"
-                        to='/login'
-                    >
-                        Login
-                    </Link>
-                </li>
-                <li>
-                    <Link
-                        title="Link to site administration panel"
-                        className="
+                                to='/login'
+                            >
+                                Login
+                            </Link>
+                        </li>
+
+                        <li>
+                            <Link
+                                title="Link to About Us page"
+                                className="
                             focus:ring-4
                             focus:ring-gray-300"
-                        to='/admin'
-                    >
-                        Admin
-                    </Link>
-                </li>
+                                to='/about'
+                            >
+                                About
+                            </Link>
+                        </li>
+
+                        <li>
+                            <Link
+                                title="Link to site administration panel"
+                                className="
+                            focus:ring-4
+                            focus:ring-gray-300"
+                                to='/admin'
+                            >
+                                Admin
+                            </Link>
+                        </li>
+                    </React.Fragment>
+                }
             </ul>
+
+            {isSmallScreen ?
+                <div
+                    // onBlur={toggleMenuOnClick}
+                    className="
+                    relative
+                    ">
+                    <button
+                        onClick={toggleMenuOnClick}
+                        // onBlur={toggleMenuOnClick}
+                    >
+                        <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 hover:text-blue-light" fill="none" viewBox="0 0 24 24"
+                             stroke="currentColor">
+                            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2}
+                                  d="M4 6h16M4 12h16M4 18h16"/>
+                        </svg>
+                    </button>
+                    {menuState ?
+                        <ul
+                            onClick={toggleMenuOnClick}
+                            className="
+                            p-4
+                            rounded
+                            shadow
+                            absolute right-0 z-10
+                            min-w-min
+                            bg-yellow border
+                            flex flex-col
+                            children:border-b-4
+                            children:border-yellow
+                            children:hover:text-blue-light
+                            children:hover:border-blue-light"
+                        >
+                            <li>
+                                <Link
+                                    title="Link to login page"
+                                    className="
+                            focus:ring-4
+                            focus:ring-gray-300"
+                                    to='/login'
+                                >
+                                    Login
+                                </Link>
+                            </li>
+                            <li>
+                                <Link
+                                    title="Link to About Us page"
+                                    className="
+                            focus:ring-4
+                            focus:ring-gray-300"
+                                    to='/about'
+                                >
+                                    About
+                                </Link>
+                            </li>
+                            <li>
+                                <Link
+                                    title="Link to site administration panel"
+                                    className="
+                            focus:ring-4
+                            focus:ring-gray-300"
+                                    to='/admin'
+                                >
+                                    Admin
+                                </Link>
+                            </li>
+                        </ul> : null}
+                </div> : null}
         </nav>
     );
 }
