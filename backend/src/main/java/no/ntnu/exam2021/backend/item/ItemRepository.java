ecdf0e428a7df54fa4c12dc20b2bd0726cfbd910
Olav Valle <olavval@stud.ntnu.no>
2021-05-12 23:49:59 +0200

    Starts work on incorporating Spring Security. Relates to issue #39

--- backend/src/main/java/no/ntnu/exam2021/backend/item/ItemRepository.java
+++ backend/src/main/java/no/ntnu/exam2021/backend/item/ItemRepository.java
@@ -1,14 +1,12 @@
 package no.ntnu.exam2021.backend.item;
 
-import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
-import org.springframework.data.rest.webmvc.RepositoryRestController;
-import org.springframework.stereotype.Repository;
 import org.springframework.web.bind.annotation.CrossOrigin;
+
+import java.util.List;
 
 @CrossOrigin
 @RepositoryRestResource(collectionResourceRel = "items", path = "items")
